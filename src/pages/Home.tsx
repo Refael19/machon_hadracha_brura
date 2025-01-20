79a649bbf0f3f4ab1801ef3bd513f9f540f44b21
Refael19 <yesh.refael@gmail.com>
2025-01-20 12:24:19 +0200

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-blue-500 mt-[100px] text-center text-5xl'>Welcome to "Machon hadracha brura"</h1>
+    </div>
+  )
+}
+
+export default Home
