92101a7b86f25db97f239f75c9a35fbbd07c8eaa
Refael19 <yesh.refael@gmail.com>
2025-01-20 15:01:07 +0200

    test netlify

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className='text-blue-500 mt-[100px] text-center text-9xl'>Welcome to "Machon hadracha brura"</h1>
+      <h1 className='text-blue-500 mt-[100px] text-center text-5xl'>Welcome to "Machon hadracha brura"</h1>
     </div>
   )
 }
