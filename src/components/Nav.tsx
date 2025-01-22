f53eb99dcd5dfe56d8f73e8baa212bf950d6e700
Refael19 <yesh.refael@gmail.com>
2025-01-22 10:14:57 +0200

    update nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,30 +3,22 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   // statics
-  const [navStatic,setNavStatic] = useState({
-    display: "",
-    width: '30%',
-    marginMenu: '100%'
-  });
+  const [isMenuOpen,setIsMenuOpen] = useState(true);
 
   // functions
-  const menuButton = () => {
-    setNavStatic((prevNav) => ({
-      display: prevNav.display === "hidden" ? "" : "hidden",
-      width: prevNav.width === "0%" ? "30%" : "0%",
-      marginMenu: prevNav.marginMenu === "0%" ? "100%" : "0%"
-    }));
+  const toogleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
   };
 
   return (
-    <nav className={`relative bg-blue-300 w-[${navStatic.width}]`}>
+    <nav className={`relative bg-blue-300 ${isMenuOpen? "w-[30%]":"w-[0%]"}`}>
       <button
-        onClick={menuButton}
-        className={`absolute w-[30px] m-[${navStatic.marginMenu}] mt-0`}
+        onClick={toogleMenu}
+        className={`absolute w-[30px] ${isMenuOpen? "m-[100%]":"m-[0%]"} mt-0`}
       >
         <img src="src\assets\icons\icons8-menu-48.png" alt="" />
       </button>
-      <div className={`flex flex-col mx-5 text-center ${navStatic.display}`}>
+      <div className={`flex flex-col mx-5 text-center ${isMenuOpen? "":"hidden"}`}>
         <NavLink
           to={"/"}
           className={({ isActive }) => (isActive ? "font-bold" : "")}
